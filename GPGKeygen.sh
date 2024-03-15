9463b3c9817992a8a091f4077bb1fa33f9629371
namay26 <namayrohatgi@gmail.com>
2024-03-16 01:53:45 +0530

    Add infinite loop

--- GPGKeygen.sh
+++ GPGKeygen.sh
@@ -13,36 +13,39 @@ bgWhite="$(tput setb 7)";
 
 clear
 echo "${bgWhite}${BLACK}                     ---------WELCOME TO GPGKeygen---------                     ${RESET}"
-echo 
-existing_key
-if [ $keyno -eq 0 ];
-then
-	echo "${GREEN}There are no keys associated with this account. Do you wish to : ${RESET}"
-	echo "${BLUE}1. Set up a new key${RESET}"
-	echo "${RED}0. Exit${RESET}"
-else
-	echo "${GREEN}There are $keyno key-s linked to this account. Do you wish to : ${RESET}"
-	echo "${BLUE}1. Set up a new key${RESET}"
-	echo "${YELLOW}2. Work with an existing key${RESET}"
-	echo "${RED}0. Exit${RESET}"
-fi
-echo "----------------------------------"
-read ans
-
-if [ $ans -eq 1 ];
-then
-	makekey=$(gpg --full-gen-key)
+exitvar=0
+while [ $exitvar -eq 0 ];
+do
+	echo 
 	existing_key
-	newkey=${keys[keyno]}
-	keywork ${newkey}
-	ext
-elif [ $ans -eq 2 ];
-then
-	listkey
-	read whckey
-	oldkey=${keys[maxkeys-whckey+1]}
-	keywork $oldkey
-	ext
-else
-	ext
-fi
+	if [ $keyno -eq 0 ];
+	then
+		echo "${GREEN}There are no keys associated with this account. Do you wish to : ${RESET}"
+		echo "${BLUE}1. Set up a new key${RESET}"
+		echo "${RED}0. Exit${RESET}"
+	else
+		echo "${GREEN}There are $keyno key-s linked to this account. Do you wish to : ${RESET}"
+		echo "${BLUE}1. Set up a new key${RESET}"
+		echo "${YELLOW}2. Work with an existing key${RESET}"
+		echo "${RED}0. Exit${RESET}"
+	fi
+	echo "----------------------------------"
+	read ans
+
+	if [ $ans -eq 1 ];
+	then
+		makekey=$(gpg --full-gen-key)
+		existing_key
+		newkey=${keys[keyno]}
+		keywork ${newkey}
+	elif [ $ans -eq 2 ];
+	then
+		listkey
+		read whckey
+		oldkey=${keys[maxkeys-whckey+1]}
+		keywork $oldkey
+	else
+		ext
+		((exitvar--))
+	fi
+done
